3b666b56104ea390c80f39abd1890f467644febd
javier.suarez <javier.suarez@nuvu.cc>
2025-04-11 14:17:54 -0500

    test, multiple lambdas

---
+++ nuv-test-get-calendar-bucket-datalake/lambda_function.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+from urllib.parse import unquote_plus
+# import loggin as logger
+
+# Initialize the S3 client to interact with S3
+s3 = boto3.client('s3')
+
+source_bucket = 'nuv-prod-calendars-datalake'  # Bucket in Account Nuvu 10
+destination_bucket = 'nuv-test-calendar-datalake'  # Bucket in Account Universidad Nacional
+
+def lambda_handler(event, context):
+
+    # logger.info(event)
+    # Get the file details from the event
+    for record in event['Records']:
+        # Extract the object key (file name)
+        bucket_name = unquote_plus(record['s3']['bucket']['name'])
+        object_key = unquote_plus(record['s3']['object']['key'])
+
+        URI = f"s3://{bucket_name}/{object_key}"
+
+        if "AREO_Data" in URI.split('/')[4]:
+        
+            try:
+                # Copy the object from the source bucket in Account A to the destination bucket in Account B
+                copy_source = {'Bucket': source_bucket, 'Key': object_key}
+                s3.copy_object(CopySource=copy_source, Bucket=destination_bucket, Key=object_key)
+
+                print(f"Successfully copied {object_key} from {source_bucket} to {destination_bucket}")
+
+            except Exception as e:
+                print(f"Error copying {object_key}: {str(e)}")
+                raise e
+        else: 
+            print(f"Skipping file {object_key} as it does not match the criteria.")
+            print('Test del flujo de GitHub')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('File copy operation completed.')
+    }
